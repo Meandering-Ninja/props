70de459ae60772f849a9d28a2c9263b20ca79d4a
Meandering-Ninja <161959791+Meandering-Ninja@users.noreply.github.com>
2024-07-29 11:14:29 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,7 @@
 import {useMemo, useState, useReducer, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-// function reducer (state,action){
-//   switch(action.type){
-//     case 'increment': return {count:state.count + 1, revcount:state.revcount -1}
-    
-//     case 'decrement': return {count : state.count -1, revcount :state.revcount +1}
-    
-//     default:return state
-//   }
 
-// }
 function Childcomp(props){
   return <div>
     <p>We are using props</p>
@@ -19,23 +10,12 @@ function Childcomp(props){
   </div>
 }
 function App() {
-  // function handlesubmiti(){
-  //   dispatch({type:'increment'})
-  // }
-  // function handlesubmitd(){
-  //   dispatch({type:'decrement'})
-  // }
-  // let [state, dispatch] = useReducer(reducer, {count:0, revcount:0})
-  // useEffect(()=>{
-  //   console.log(state.count +'  '+ state.revcount)
-  // },[state.count,state.revcount])
+  
   return (
     <div className="App">
 {
   <Childcomp name ="Saif" occ ="Developer"/>
-/*       
-      <input type='button' value='Increment' onClick={handlesubmiti}></input>
-      <input type='button' value='Decrement' onClick={handlesubmitd}></input> */}
+
     </div>
   );
 }
